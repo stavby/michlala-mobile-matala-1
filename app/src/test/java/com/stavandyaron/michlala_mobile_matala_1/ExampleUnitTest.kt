01afd55a4c6da1d377b2ff51af5d88cfef6c7af4
stavby <stav134679@gmail.com>
2025-01-18 13:00:53 +0200

    init project

---
+++ app/src/test/java/com/stavandyaron/michlala_mobile_matala_1/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.stavandyaron.michlala_mobile_matala_1
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
