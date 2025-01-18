30103a395e148ecfcbd522e19a9b55e56c8ad939
stavby <stav134679@gmail.com>
2025-01-18 13:31:47 +0200

    change gradle stuff

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.stavandyaron.michlala_mobile_matala_1"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.stavandyaron.michlala_mobile_matala_1"
